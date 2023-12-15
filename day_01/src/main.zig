abac393d0126ccbe97f96f5c235e8fc182e78aaa
TamLC <tamlc7@fpt.com.vn>
2023-12-15 18:26:21 +0700

    wip: Day 01 - Part 02

--- day_01/src/main.zig
+++ day_01/src/main.zig
@@ -1,16 +1,17 @@
 const std = @import("std");
 const fs = std.fs;
 
+const p1 = @import("part-one.zig");
+const p2 = @import("part-two.zig");
+
 const MAX_BUFFER_SIZE = 1024 * 4; // 4 Kilobytes
 
-const p1 = @import("part-one.zig");
+// Create memory allocator
+var buffer: [MAX_BUFFER_SIZE]u8 = undefined;
+var fba = std.heap.FixedBufferAllocator.init(&buffer);
+const allocator = fba.allocator();
 
 pub fn main() !void {
-    // Create memory allocator
-    var buffer: [MAX_BUFFER_SIZE]u8 = undefined;
-    var fba = std.heap.FixedBufferAllocator.init(&buffer);
-    const allocator = fba.allocator();
-
     // stdout is for the actual output of your application, for example if you
     // are implementing gzip, then only the compressed bytes should be sent to
     // stdout, not any debugging messages.
@@ -21,12 +22,20 @@ pub fn main() !void {
     const inputPath: []const u8 = "input/data.txt";
 
     try stdout.print("Result for part 01: {d}\n", .{try p1.solve(inputPath, &allocator)});
+    try stdout.print("Result for part 02: {d}\n", .{try p2.solve(inputPath, &allocator)});
     try bw.flush(); // don't forget to flush!
 }
 
 test "test part 01 with sample data" {
     const expected: u32 = 142;
     const actual = try p1.solve("input/sample_1.txt", &std.testing.allocator);
+
+    try std.testing.expectEqual(expected, actual);
+}
+
+test "test part 02 with sample data" {
+    const expected: u32 = 142;
+    const actual = try p2.solve("input/sample_2.txt", &std.testing.allocator);
 
     try std.testing.expectEqual(expected, actual);
 }
--- day_01/src/part-two.zig
+++ day_01/src/part-two.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const fs = std.fs;
 
+const Allocator = std.mem.Allocator;
+
 // String to num map
 // O
 // └─── N
@@ -36,13 +38,100 @@ const fs = std.fs;
 //      └─── N
 //           └─── E
 
-pub fn solve(filepath: []const u8, comptime allocator: *const std.mem.Allocator) !u32 {
+pub fn Node(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        num: ?T,
+        next: *[]Self,
+
+        fn dealloc(self: *Self, allocator: Allocator) void {
+            // Deallocate following start_node
+            if (self.next != null) {
+                self.next.len
+                self.next.dealloc(allocator);
+            }
+
+            // Deallocate current start_node
+            allocator.free(self.next);
+            self.* = null;
+        }
+    };
+}
+
+const NumericTree = struct {
+    const Self = @This();
+
+    start_node: *Node(u8),
+    allocator: Allocator,
+
+    pub fn init(allocator: Allocator) Allocator.Error!Self {
+        var start_node: *Node(u8) = try allocator.create(Node(u8));
+        // start_node.* = .{ .num = null, .next = try allocator.alloc(Node(u8), 26) };
+
+        const words = "one|two|three|four|five|six|seven|eight|nine|";
+        var current_num = 1;
+        var iter = start_node;
+        for (words) |char| {
+            std.debug.print("{c}", .{char});
+            switch (char) {
+                '|' => {
+                    iter.*.num = current_num;
+                    iter = start_node;
+                    current_num += 1;
+                },
+                else => |x| {
+                    iter.*.next = try allocator.alloc(Node(u8), 26);
+                    iter.*.next[x] = try allocator.create(Node(u8));
+                    iter = &(iter.*.next[x]);
+                },
+            }
+        }
+
+        return .{ .start_node = start_node, .allocator = allocator };
+    }
+
+    fn deinit(self: *Self) void {
+        if (self.start_node == null) return;
+
+        self.start_node.*.dealloc(self.allocator);
+        self.allocator.destroy(self.start_node);
+        self.start_node = null;
+    }
+
+    // build_tree: {
+    // const words = "one|two|three|four|five|six|seven|eight|nine";
+
+    // var result = NumericTree{ .next = [26]NumericTree };
+    // var current_num = 1;
+    // var iter: *NumericTree = &result;
+    // for (words) |char| {
+    //     std.debug.print("{c}", .{char});
+    //     switch (char) {
+    //         '|' => {
+    //             iter.* = NumericTree{ .num = current_num };
+    //             iter = &result;
+    //             current_num += 1;
+    //         },
+    //         else => |x| {
+    //             iter.*.next[x] = NumericTree{ .next = [26]NumericTree };
+    //             iter = iter.*.next[x];
+    //         },
+    //     }
+    // }
+
+    // break :build_tree result;
+};
+
+pub fn solve(filepath: []const u8, comptime allocator: *const Allocator) !u32 {
     const file = try fs.cwd().openFile(filepath, .{ .mode = .read_only });
     var buffer = try allocator.alloc(u8, 1024);
     defer {
         file.close();
         allocator.free(buffer);
     }
+
+    std.debug.print("{d}", .{tree['o']['n']['e']});
 
     var total: u32 = 0;
     var lead: u8 = 0;
@@ -55,3 +144,5 @@ pub fn solve(filepath: []const u8, comptime allocator: *const std.mem.Allocator)
 
     return total;
 }
+
+test "build tree" {}
